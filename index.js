20edeada022df76d990a0f12017a779b8890c696
zhuxucome <zhuxucome@126.com>
2022-07-14 20:21:45 +0800

    第二次提交

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/*
+ * @Author: zhuxucome zhuxucome@126.com
+ * @Date: 2022-07-14 20:16:14
+ * @LastEditors: zhuxucome zhuxucome@126.com
+ * @LastEditTime: 2022-07-14 20:18:39
+ * @FilePath: \vscode\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 hello
+second
